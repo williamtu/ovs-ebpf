cb944383fb0301fcca9a351374b2c57af58f6a3a
William Tu <u9012063@gmail.com>
2018-06-20 12:38:21 -0700

    bpf: implement OVS BPF datapath.
    This patch adds the OVS-eBPF datapath implementation for dpif-bpf.
    Three stages are added: parse, lookup, and actions.  Each stages are
    tail called to the next stage.  When executing multiple actions,
    the current action also tail calls the subsequent action, based on
    the result of flow table lookup.
    
    The protocol headers are auto-generated and defined at generated_headers.h.
    The bpf_flow_key is extracted using the P4-to-eBPF compiler from
    the bcc project.  A couple of manual tweaks are required, see parser.h.
    
    Signed-off-by: William Tu <u9012063@gmail.com>
    Signed-off-by: Yifeng Sun <pkusunyifeng@gmail.com>
    Signed-off-by: Joe Stringer <joe@ovn.org>
    Co-authored-by: Joe Stringer <joe@ovn.org>
    Co-authored-by: Yifeng Sun <pkusunyifeng@gmail.com>

---
+++ bpf/lookup.h
@@ -0,0 +1,227 @@
+/*
+ * Copyright (c) 2016, 2017, 2018 Nicira, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ */
+#include <openvswitch/compiler.h>
+#include "ovs-p4.h"
+#include "api.h"
+#include "helpers.h"
+#include "maps.h"
+
+/* eBPF executes actions by tailcall because eBPF doesn't support for-loop and
+ * unroll produces oversized code.
+ *
+ * Each action handler uses current packet's key to look for the next action.
+ * However, the key can be changed by some actions like hash, so a stable
+ * key is kept in an eBPF map named percpu_executing_key. In action handler,
+ * firstly, the stable key is got from percpu_executing_key, then it is used
+ * to look up the actions being executed. skb->cb[OVS_CB_ACT_IDX] points to
+ * next action.
+ */
+static inline void ovs_execute_actions(struct __sk_buff *skb,
+                                       struct bpf_action *action)
+{
+    enum ovs_action_attr type;
+    type = action->type;
+
+    printt("action type %d\n", type);
+
+	/* note: this isn't a for loop, tail call won't return. */
+    switch (type) {
+    case OVS_ACTION_ATTR_UNSPEC:
+        printt("end of action processing\n");
+        break;
+    case OVS_ACTION_ATTR_OUTPUT:
+        printt("output action port = %d\n", action->u.out.port);
+        break;
+    case OVS_ACTION_ATTR_USERSPACE:
+        printt("userspace action, len = %d, ifindex = %d upcall back\n",
+               action->u.userspace.nlattr_len, ovs_cb_get_ifindex(skb));
+        break;
+    case OVS_ACTION_ATTR_SET:
+        printt("set action, remote ipv4 = %x, is_set = %d\n",
+               action->u.tunnel.remote_ipv4, action->is_set);
+        break;
+    case OVS_ACTION_ATTR_PUSH_VLAN:
+        printt("vlan push tci %d\n", action->u.push_vlan.vlan_tci);
+        break;
+    case OVS_ACTION_ATTR_POP_VLAN:
+        printt("vlan pop\n");
+        break;
+    case OVS_ACTION_ATTR_RECIRC:
+        printt("recirc\n");
+        break;
+    case OVS_ACTION_ATTR_HASH:
+        printt("hash\n");
+        break;
+    case OVS_ACTION_ATTR_SET_MASKED:
+        printt("set masked\n");
+        break;
+    case OVS_ACTION_ATTR_CT:
+        printt("ct\n");
+        break;
+    case OVS_ACTION_ATTR_TRUNC:
+        printt("truncate\n");
+        break;
+	case OVS_ACTION_ATTR_SAMPLE:       /* Nested case OVS_SAMPLE_ATTR_*. */
+	case OVS_ACTION_ATTR_PUSH_MPLS:    /* struct ovs_action_push_mpls. */
+	case OVS_ACTION_ATTR_POP_MPLS:     /* __be16 ethertype. */
+	case OVS_ACTION_ATTR_PUSH_ETH:     /* struct ovs_action_push_eth. */
+	case OVS_ACTION_ATTR_POP_ETH:      /* No argument. */
+	case OVS_ACTION_ATTR_CT_CLEAR:     /* No argument. */
+	case OVS_ACTION_ATTR_PUSH_NSH:     /* Nested case OVS_NSH_KEY_ATTR_*. */
+	case OVS_ACTION_ATTR_POP_NSH:      /* No argument. */
+#ifndef __KERNEL__
+	case OVS_ACTION_ATTR_TUNNEL_PUSH:   /* struct ovs_action_push_tnl*/
+	case OVS_ACTION_ATTR_TUNNEL_POP:    /* u32 port number. */
+	case OVS_ACTION_ATTR_CLONE:         /* Nested case OVS_CLONE_ATTR_*.  */
+	case OVS_ACTION_ATTR_METER:         /* u32 meter number. */
+#endif
+	case __OVS_ACTION_ATTR_MAX:
+#ifdef __KERNEL__
+	case OVS_ACTION_ATTR_SET_TO_MASKED: /* Kernel module internal masked
+					* set action converted from
+					* case OVS_ACTION_ATTR_SET. */
+#endif
+    default:
+        printt("ERR: action type %d not supportedn", type);
+        break;
+    }
+
+    bpf_tail_call(skb, &tailcalls, type);
+
+    /* OVS_NOT_REACHED */
+    return;
+}
+
+static inline void
+stats_account(enum ovs_bpf_dp_stats index)
+{
+    uint32_t stat = 1;
+    uint64_t *value;
+
+    value = map_lookup_elem(&datapath_stats, &index);
+    if (value) {
+        __sync_fetch_and_add(value, stat);
+    }
+}
+
+/* OVS revalidator thread reads each entry in eBPF map
+ * (flow_table and dp_flow_stats), reports to OpenFlow
+ * table statistics, and decide to remove/keep the entry
+ * by comparing its timestamp.
+ */
+static inline void
+flow_stats_account(struct ebpf_headers_t *headers,
+                   struct ebpf_metadata_t *mds,
+                   size_t bytes)
+{
+    struct bpf_flow_key flow_key;
+    struct bpf_flow_stats *flow_stats;
+
+    flow_key.headers = *headers;
+    flow_key.mds = *mds;
+
+    flow_stats = bpf_map_lookup_elem(&dp_flow_stats, &flow_key);
+    if (!flow_stats) {
+        struct bpf_flow_stats s = {0, 0, 0};
+        int err;
+
+        printt("flow not found in flow stats, first install\n");
+        s.packet_count = 1;
+        s.byte_count = bytes;
+        s.used = bpf_ktime_get_ns() / (1000*1000); /* msec */
+        err = bpf_map_update_elem(&dp_flow_stats, &flow_key, &s, BPF_ANY);
+        if (err) {
+            return;
+        }
+    } else {
+        flow_stats->packet_count += 1;
+        flow_stats->byte_count += bytes;
+        flow_stats->used = bpf_ktime_get_ns() / (1000*1000); /* msec */
+        printt("current: packets %d count %d ts %d\n",
+            flow_stats->packet_count, flow_stats->byte_count, flow_stats->used);
+    }
+
+    return;
+}
+
+static inline struct bpf_action_batch *
+ovs_lookup_flow(struct ebpf_headers_t *headers,
+                struct ebpf_metadata_t *mds)
+{
+    struct bpf_flow_key flow_key;
+
+    flow_key.headers = *headers;
+    flow_key.mds = *mds;
+
+    return bpf_map_lookup_elem(&flow_table, &flow_key);
+}
+
+__section_tail(MATCH_ACTION_CALL)
+static int lookup(struct __sk_buff* skb OVS_UNUSED)
+{
+    struct bpf_action_batch *action_batch;
+    struct ebpf_headers_t *headers;
+    struct ebpf_metadata_t *mds;
+
+    headers = bpf_get_headers();
+    if (!headers) {
+        printt("no packet header found\n");
+        ERR_EXIT();
+    }
+
+    mds = bpf_get_mds();
+    if (!mds) {
+        printt("no packet metadata found\n");
+        ERR_EXIT();
+    }
+
+    /* LOOKUP */
+    action_batch = ovs_lookup_flow(headers, mds);
+    if (!action_batch) {
+        printt("no action found, upcall to userspace\n");
+        bpf_tail_call(skb, &tailcalls, UPCALL_CALL);
+
+        /* OVS_NOT_REACHED */
+        return TC_ACT_OK;
+    } else {
+        /* DP Stats Update */
+        stats_account(OVS_DP_STATS_HIT);
+        /* Flow Stats Update */
+        flow_stats_account(headers, mds, skb->len);
+    }
+
+    /* Hit verifier limit when moving declaration up. */
+    struct bpf_flow_key flow_key;
+    flow_key.headers = *headers;
+    flow_key.mds = *mds;
+    int index = 0;
+    int error = bpf_map_update_elem(&percpu_executing_key, &index,
+                                    &flow_key, BPF_ANY);
+    if (error) {
+        printt("update percpu_executing_key failed: %d\n", error);
+        return TC_ACT_OK;
+    }
+
+    /* the subsequent actions will be tail called. */
+    ovs_execute_actions(skb, &action_batch->actions[0]);
+
+    printt("ERROR: tail call fails\n");
+
+    /* OVS_NOT_REACHED */
+    return TC_ACT_OK;
+}
---
+++ bpf/maps.h
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2016, 2017, 2018 Nicira, Inc.
+ *
+ * This file is offered under your choice of two licenses: Apache 2.0 or GNU
+ * GPL 2.0 or later.  The permission statements for each of these licenses is
+ * given below.  You may license your modifications to this file under either
+ * of these licenses or both.  If you wish to license your modifications under
+ * only one of these licenses, delete the permission text for the other
+ * license.
+ *
+ * ----------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ * ----------------------------------------------------------------------
+ */
+
+#ifndef BPFMAP_OPENVSWITCH_H
+#define BPFMAP_OPENVSWITCH_H 1
+
+#include "api.h"
+#include "openvswitch.h"
+#include "ovs-p4.h"
+
+/* ovs-vswitchd as a writer will update these maps.
+ * bpf datapath as reader lookup and processes */
+
+/* FIXME: copy from iproute2 */
+enum {
+    BPF_MAP_ID_PROTO,
+    BPF_MAP_ID_QUEUE,
+    BPF_MAP_ID_DROPS,
+    BPF_MAP_ID_ACTION,
+    BPF_MAP_ID_INGRESS,
+    __BPF_MAP_ID_MAX,
+#define BPF_MAP_ID_MAX  __BPF_MAP_ID_MAX
+};
+
+/* A bpf flow key is extracted from the
+ * parser.h and saved in
+ *  1) percpu_headers, and
+ *  2) percpu_metadata
+ * Access: BPF is the only writer/reader
+ */
+BPF_PERCPU_ARRAY(percpu_headers,
+        0,
+        sizeof(struct ebpf_headers_t),
+        0,
+        1
+);
+BPF_PERCPU_ARRAY(percpu_metadata,
+        0,
+        sizeof(struct ebpf_metadata_t),
+        0,
+        1
+);
+
+/* BPF flow tale
+ * Access: BPF is the reader for lookup,
+ *         ovs-vswitchd is the writer
+ */
+BPF_HASH(flow_table,
+        0,
+        sizeof(struct bpf_flow_key),
+        sizeof(struct bpf_action_batch),
+        0,
+        256
+);
+
+/* BPF flow stats table
+ * Access: BPF is the writer for updating,
+ *         ovs-vswitchd/revalidator is the reader
+ */
+BPF_HASH(dp_flow_stats,
+        0,
+        sizeof(struct bpf_flow_key),
+        sizeof(struct bpf_flow_stats),
+        0,
+        256
+);
+
+/*
+ * Map for implementing the upcall, which forwards the
+ * first packet (lookup misses) to ovs-vswitchd
+ */
+BPF_PERF_OUTPUT(upcalls, 0);
+
+
+/* BPF datapath stats
+ * Access: BPF is the writer,
+ *         ovs-vswitchd is the reader
+ * XXX: switch to percpu to improve performance
+ */
+BPF_ARRAY(datapath_stats,
+        0,
+        sizeof(uint64_t),
+        0,
+        __OVS_DP_STATS_MAX
+);
+
+/* Global tail call map:
+ *  index  0-31 for actions (OVS_ACTION_ATTR_*)
+ *  index 32-63 for others
+ */
+BPF_PROG_ARRAY(tailcalls,
+        0,
+        0,
+        64
+);
+
+/* A dedicated action list for downcall packet.
+ * Access: ovs-vswitch is the writer,
+ *         BPF is the reader
+ */
+BPF_ARRAY(execute_actions,
+        0,
+        sizeof(struct bpf_action_batch),
+        0,
+        1
+);
+
+/* A dedicated key for downcall packet.
+ * Access: ovs-vswitch is the writer,
+ *         BPF is the reader
+ */
+BPF_PERCPU_ARRAY(percpu_executing_key,
+        0,
+        sizeof(struct bpf_flow_key),
+        0,
+        1
+);
+
+struct ebpf_headers_t;
+struct ebpf_metadata_t;
+
+static inline struct ebpf_headers_t *bpf_get_headers()
+{
+    int ebpf_zero = 0;
+    return bpf_map_lookup_elem(&percpu_headers, &ebpf_zero);
+}
+
+static inline struct ebpf_metadata_t *bpf_get_mds()
+{
+    int ebpf_zero = 0;
+    return bpf_map_lookup_elem(&percpu_metadata, &ebpf_zero);
+}
+
+#endif /* BPFMAP_OPENVSWITCH_H */
